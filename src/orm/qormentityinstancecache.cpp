d00320b98e427cb36f79ba69d425e144cf4375cc
Dmitriy Purgin <dpurgin@gmail.com>
2019-10-22 22:18:45 +0200

    license header added

--- src/orm/qormentityinstancecache.cpp
+++ src/orm/qormentityinstancecache.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2019 Dmitriy Purgin <dmitriy.purgin@sequality.at>
+ * Copyright (C) 2019 sequality software engineering e.U. <office@sequality.at>
+ *
+ * This file is part of QtOrm library.
+ *
+ * QtOrm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QtOrm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with QtOrm.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "qormentityinstancecache.h"
 #include "qormglobal_p.h"
 #include "qormmetadata.h"
@@ -22,7 +42,7 @@ private slots:
 private:
     QHash<QObject*, ObjectId> m_cache;
     QMap<ObjectId, QObject*> m_byObjectId;
-    QSet<const QObject*> m_modifiedInstances;
+    QSet<const QObject*> m_modifiedInstances;    
 };
 
 void QOrmEntityInstanceCachePrivate::onEntityInstanceChanged()
